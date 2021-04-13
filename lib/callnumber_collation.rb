76351afa967cb573bffa1ec5c9e73da3d605b0d9
Bill Dueber <bill@dueber.com>
2021-04-13 14:49:19 -0400

    First commit

---
+++ lib/callnumber_collation.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "callnumber_collation/version"
+
+module CallnumberCollation
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/callnumber_collation/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CallnumberCollation
+  VERSION = "0.1.0"
+end
